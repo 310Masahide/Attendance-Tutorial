b978daf0e546e8b29d9e4a30c95179beb05b493f
SatohMasahide <m55841029@gmail.com>
2026-05-29 20:18:14 +0900

    日時の設定を追加

--- attendance_app/config/application.rb
+++ attendance_app/config/application.rb
@@ -6,10 +6,11 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module App
+module AttendanceApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
+    config.time_zone = 'Asia/Tokyo'
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
