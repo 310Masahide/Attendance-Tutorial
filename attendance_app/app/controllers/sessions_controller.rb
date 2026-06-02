174b549ea8b80fcee9e9cb823c1fb94b95202dc1
SatohMasahide <m55841029@gmail.com>
2026-06-02 21:03:39 +0900

    Finish full-fledged-login

--- attendance_app/app/controllers/sessions_controller.rb
+++ attendance_app/app/controllers/sessions_controller.rb
@@ -4,18 +4,19 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-       log_in user
-       redirect_to user
-    else
-      flash.now[:danger] = '認証に失敗しました。'
-      render 'new', status: :unprocessable_entity
-    end
+  user = User.find_by(email: params[:session][:email].downcase)
+  if user && user.authenticate(params[:session][:password])
+    log_in user
+    params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+    redirect_to user
+  else
+    flash.now[:danger] = '認証に失敗しました。'
+    render :new
   end
+end
 
   def destroy
-    log_out
+    log_out if logged_in?
     flash[:success] = 'ログアウトしました。'
     redirect_to root_url
   end
